bba7e71b3bc5e53a267d65c375c674554e65a448
zjfeihu <zjfeihu@126.com>
2019-08-30 08:00:56 +0800

    修改eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,5 @@
 module.exports = {
-  env: {
-    commonjs: true,
-    es6: true
-  },
   extends: [
-    'standard', 'plugin:vue/essential'
+    '@smartx/eslint-config-tentative'
   ],
-  globals: {
-
-  },
-  parserOptions: {
-    ecmaVersion: 2018
-  },
-  rules: {
-    ...require('./rules/basic'),
-    ...require('./rules/vue'),
-  }
 }
